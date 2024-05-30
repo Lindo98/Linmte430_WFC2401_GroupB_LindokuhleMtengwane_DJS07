eb8734524712b76d0e3c7f6e46490dbdda0fb991
Lindokuhle Mtengwane <lindokuhlemtengwane@gmail.com>
2024-05-30 20:31:49 +0200

    added vite

--- meme-generator/src/components/header.js
+++ meme-generator/src/components/header.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function Header() {
   return (
     <header>
+      <img src="https://via.placeholder.com/150" alt="logo" />
       <h1>Header component</h1>
+      <h4>React Course - Project 3</h4>
     </header>
   );
 }
